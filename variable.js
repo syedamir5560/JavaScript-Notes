01771444eea21a2fa520fc52cefd3f3b407ec8ae
Sayyed Aamir Ali <133253595+syedamir5560@users.noreply.github.com>
2023-08-12 12:24:11 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,91 +1,114 @@
-/*
-*********************************VARIABLES**************************************************
+# JavaScript Notes
 
-A variable is a label that references a value like a number or string.Before using a variable, you need to declare it. variables are used to store a data.
+## Variables
 
-To declare a variable, you use the var keyword followed by the variable name as follows: */
+A variable is a label that references a value like a number or string. Before using a variable, you need to declare it.
 
-var message;  //declaration
-message = "variable";  //initialization
-console.log("message");
+To declare a variable, you use the `var` keyword followed by the variable name as follows:
 
-/*JavaScript is a dynamically typed language.This means that you don’t need to specify the variable’s type in the declaration like other static typed languages such as Java or C#.
-Starting in ES6, you can use the let keyword to declare a variable like this:*/
+```javascript
+var message;  // declaration
+message = "variable";  // initialization
+console.log(message);
 
-let message;
-message = "Hello";
+JavaScript is a dynamically typed language. This means that you don’t need to specify the variable’s type in the declaration like other static typed languages such as Java or C#.
+Starting in ES6, you can use the let keyword to declare a variable like this:
+let message = "Hello";
 
-/*The main difference between `var` and `let` is that `var` is a ** global variable **, while `let` is a ** block - scoped variable **.This means that `var` can be accessed from anywhere in the code, while `let` can only be accessed within the block in which it is declared.*/
+The main difference between var and let is that var is a global variable, while let is a block-scoped variable. This means that var can be accessed from anywhere in the code, while let can only be accessed within the block in which it is declared.
+var x = 10; // This is a global variable
 
+function myFunction() {
+  let y = 20; // This is a block-scoped variable
+
+  document.write(x); // This will print 10
+  document.write(y); // This will also print 20
+}
+
+myFunction();
+
+Constants
+
+A constant holds a value that doesn’t change. To declare a constant, you use the const keyword. When defining a constant, you need to initialize it with a value. For example:
+const workday = 5;
+Once defining a constant, you cannot change its value.
+
+The following example attempts to change the value of the workday constant to 4 and causes an error:
+workday = 2;
+Sure, here is the provided JavaScript code converted into raw Markdown format for a GitHub readme file:
+
+markdown
+
+# JavaScript Notes
+
+## Variables
+
+A variable is a label that references a value like a number or string. Before using a variable, you need to declare it.
+
+To declare a variable, you use the `var` keyword followed by the variable name as follows:
+
+```javascript
+var message;  // declaration
+message = "variable";  // initialization
+console.log(message);
+
+JavaScript is a dynamically typed language. This means that you don’t need to specify the variable’s type in the declaration like other static typed languages such as Java or C#.
+
+Starting in ES6, you can use the let keyword to declare a variable like this:
+
+javascript
+
+let message = "Hello";
+
+The main difference between var and let is that var is a global variable, while let is a block-scoped variable. This means that var can be accessed from anywhere in the code, while let can only be accessed within the block in which it is declared.
+
+javascript
 
 var x = 10; // This is a global variable
 
 function myFunction() {
-        let y = 20; // This is a block-scoped variable
+  let y = 20; // This is a block-scoped variable
 
-        document.write(x); // This will print 10
-        document.write(y); // This will also print 20
+  document.write(x); // This will print 10
+  document.write(y); // This will also print 20
 }
 
-/* myFunction();
-var is feature of es1 and let is feature of es6
+myFunction();
 
-var can be declare multiple times you have two var variable with same name but you cannot have two same let variable in your code*/
+Constants
 
-var x = 10;
-var x = 200;
+A constant holds a value that doesn’t change. To declare a constant, you use the const keyword. When defining a constant, you need to initialize it with a value. For example:
 
-console.log(x); // This will print 200
-
-let y = 10;
-let y = 20; // This will throw an error
-console.log(y);
-
-/* in general, it is recommended to use `let` instead of `var` because `let` is more explicit and prevents some common errors.
-
-        Constants
-
-A constant holds a value that doesn’t change.To declare a constant, you use the const keyword.When defining a constant, you need to initialize it with a value.For example:
+javascript
 
 const workday = 5;
-Code language: JavaScript(javascript)
 
 Once defining a constant, you cannot change its value.
 
 The following example attempts to change the value of the workday constant to 4 and causes an error:
 
+javascript
+
 workday = 2;
 
-functions to intract with user 
-                        1. alert() - show message.*/
-alert(document.write);
+User Interaction Functions
 
-/* 2.prompt() - show a message, input text.it return the text on ok or if cancel button or esc clicked, null */
+    alert() - show a message.
+            alert(document.write);
+prompt() - show a message, input text. It returns the text on OK or if the cancel button or Esc is clicked, null.
 
-var age = prompt('enter age');
-if (age != nulll) {
-        document.write(`age is ${age}`);
+        var age = prompt('enter age');
+if (age != null) {
+    document.write(`age is ${age}`);
+} else {
+    document.write('age was blanked');
 }
-else {
-        document.write('age was blanked');
-}
 
-/* 3.confirm() - show a message confirm with ok or cancel.it will return true on  ok or false on  cancel. */
-
-let res = confirm('are you sure you want delete');
+confirm() - show a message confirm with OK or Cancel. It will return true on OK or false on Cancel.
+        let res = confirm('are you sure you want to delete');
 
 if (res) {
-        document.write('deleted');
+    document.write('deleted');
+} else {
+    document.write('not deleted');
 }
-else {
-        document.write('not deleted');
-}
-
-
-
-
-
-
-
-
-
